fa3339a6fe4d32d7cb840e65f2b40776fa5ee63b
pjhartzell <preston.hartzell@gmail.com>
2026-01-27 12:57:50 -0600

    Update DisplayActionsSection component

--- src/components/Search/Sections/DisplayActionsSection/DisplayActionsSection.jsx
+++ src/components/Search/Sections/DisplayActionsSection/DisplayActionsSection.jsx
@@ -1,69 +1,52 @@
 import React from 'react'
 import './DisplayActionsSection.css'
 import { useSelector, useDispatch } from 'react-redux'
-import {
-  setshowSearchByGeom,
-  setautoCenterOnItemChanged
-} from '../../../../redux/slices/mainSlice'
-import { Stack, Switch } from '@mui/material'
+import { setshowSearchByGeom, setautoCenterOnItemChanged } from '../../../../redux/slices/mainSlice'
 import Section from '../Section/Section'
 import ViewSelector from '../../../ViewSelector/ViewSelector'
+import Checkbox from '../../../Checkbox/Checkbox'
 import { newSearch } from '../../../../utils/searchHelper'
 
 const DisplayActionsSection = () => {
   const dispatch = useDispatch()
-  const _appConfig = useSelector((state) => state.mainSlice.appConfig)
-  const _autoCenterOnItemChanged = useSelector(
+  const appConfig = useSelector((state) => state.mainSlice.appConfig)
+  const autoCenterOnItemChanged = useSelector(
     (state) => state.mainSlice.autoCenterOnItemChanged
   )
-  const mosaicTilerURL = _appConfig.MOSAIC_TILER_URL || ''
+  const mosaicTilerURL = appConfig.MOSAIC_TILER_URL || ''
 
-  function processSearchBtn() {
+  const handleSearchClick = () => {
     newSearch()
     dispatch(setshowSearchByGeom(false))
   }
 
-  function updateAutoCenterState() {
-    dispatch(setautoCenterOnItemChanged(!_autoCenterOnItemChanged))
+  const handleAutoZoomChange = (e) => {
+    dispatch(setautoCenterOnItemChanged(e.target.checked))
   }
 
-  // Determine if we should show the section heading
-  // Hide heading if only Search button is visible
   const hasViewMode = !!mosaicTilerURL
-  const hasAutoZoom = !!_appConfig.SHOW_ITEM_AUTO_ZOOM
+  const hasAutoZoom = !!appConfig.SHOW_ITEM_AUTO_ZOOM
   const showSectionHeading = hasViewMode || hasAutoZoom
 
   return (
-    <Section 
-      heading={showSectionHeading ? "Display & Actions" : null} 
+    <Section
+      heading={showSectionHeading ? "Display & Actions" : null}
       className="DisplayActionsSection"
     >
-      <div className="DisplayActionsSection__content">
-        {mosaicTilerURL && (
-          <div className="searchContainer viewSelectorComponent">
-            <ViewSelector />
-          </div>
-        )}
-        {_appConfig.SHOW_ITEM_AUTO_ZOOM && (
-          <div className="searchContainer viewSelectorComponent">
-            <Stack className="searchFilterContainer">
-              <label htmlFor="ItemAutoSearch">Item Auto-Zoom</label>
-              <Switch
-                checked={_autoCenterOnItemChanged}
-                onChange={() => updateAutoCenterState()}
-              />
-            </Stack>
-          </div>
-        )}
-        <div className="searchButtonContainer">
-          <button
-            className={`actionButton searchButton`}
-            onClick={() => processSearchBtn()}
-          >
-            Search
-          </button>
-        </div>
-      </div>
+      {mosaicTilerURL && <ViewSelector />}
+      {appConfig.SHOW_ITEM_AUTO_ZOOM && (
+        <Checkbox
+          label="Item Auto-Zoom"
+          checked={autoCenterOnItemChanged}
+          onChange={handleAutoZoomChange}
+        />
+      )}
+      <button
+        className="actionButton searchButton"
+        onClick={handleSearchClick}
+      >
+        Search
+      </button>
     </Section>
   )
 }
