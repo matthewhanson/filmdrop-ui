6b87c4c4ef3fd464a982c0598ef4eeb5853f6169
pjhartzell <preston.hartzell@gmail.com>
2026-01-27 11:46:37 -0600

    Update DateTimeRangeSelector component

--- src/components/DateTimeRangeSelector/DateTimeRangeSelector.jsx
+++ src/components/DateTimeRangeSelector/DateTimeRangeSelector.jsx
@@ -1,168 +1,176 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import './DateTimeRangeSelector.css'
 import { useSelector, useDispatch } from 'react-redux'
-import 'react-tooltip/dist/react-tooltip.css'
 import { setSearchDateRangeValue } from '../../redux/slices/mainSlice'
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import dayjs from 'dayjs'
 import utc from 'dayjs/plugin/utc'
-import { convertToUTC } from '../../utils/datetime'
+import Card from '../Card/Card'
+
+dayjs.extend(utc)
 
 const DateTimeRangeSelector = () => {
-  dayjs.extend(utc)
   const dispatch = useDispatch()
-  const _selectedCollectionData = useSelector(
+  const selectedCollectionData = useSelector(
     (state) => state.mainSlice.selectedCollectionData
   )
-  const _hasCollectionChanged = useSelector(
-    (state) => state.mainSlice.hasCollectionChanged
-  )
-  const _SearchDateRangeValue = useSelector(
+  const searchDateRangeValue = useSelector(
     (state) => state.mainSlice.searchDateRangeValue
   )
-  const _hasLeftPanelTabChanged = useSelector(
-    (state) => state.mainSlice.hasLeftPanelTabChanged
-  )
-  const [startDate, setstartDate] = useState(_SearchDateRangeValue[0])
-  const [endDate, setendDate] = useState(_SearchDateRangeValue[1])
 
+  // Parse dates from Redux (single source of truth)
+  const startDate = useMemo(
+    () => new Date(searchDateRangeValue[0]),
+    [searchDateRangeValue]
+  )
+  const endDate = useMemo(
+    () => new Date(searchDateRangeValue[1]),
+    [searchDateRangeValue]
+  )
+
+  // Memoized helper text for collection extent
+  const collectionExtentText = useMemo(() => {
+    if (!selectedCollectionData?.extent?.temporal?.interval?.[0]?.[0]) {
+      return null
+    }
+
+    const startExtent = selectedCollectionData.extent.temporal.interval[0][0]
+    const endExtent = selectedCollectionData.extent.temporal.interval[0][1]
+
+    const startFormatted = dayjs(startExtent).format('YYYY-MM-DD')
+    const endFormatted = endExtent
+      ? dayjs(endExtent).format('YYYY-MM-DD')
+      : 'Present'
+
+    return `Available: ${startFormatted} to ${endFormatted}`
+  }, [selectedCollectionData])
+
+  // When collection changes, validate and update date range if needed
   useEffect(() => {
-    if (!_selectedCollectionData) {
+    if (!selectedCollectionData?.extent?.temporal?.interval?.[0]) {
       return
     }
 
-    const currentDateUTCString = dayjs(new Date()).utc().startOf('day').format()
-    const collectionEndDateOrCurrentLocal =
-      _selectedCollectionData.extent.temporal.interval[0][1] !== null
-        ? convertToUTC(_selectedCollectionData.extent.temporal.interval[0][1])
-        : convertToUTC(currentDateUTCString)
+    const collectionStart = selectedCollectionData.extent.temporal.interval[0][0]
+    const collectionEnd = selectedCollectionData.extent.temporal.interval[0][1]
 
-    // if temporal range not in last two week on init, set to match collection
-    if (!_hasCollectionChanged) {
-      if (
-        (startDate <
-          convertToUTC(
-            _selectedCollectionData.extent.temporal.interval[0][0]
-          ) &&
-          endDate <
-            convertToUTC(
-              _selectedCollectionData.extent.temporal.interval[0][0]
-            )) ||
-        (startDate > collectionEndDateOrCurrentLocal &&
-          endDate > collectionEndDateOrCurrentLocal)
-      ) {
-        setstartDate(
-          convertToUTC(_selectedCollectionData.extent.temporal.interval[0][0])
-        )
-        setendDate(collectionEndDateOrCurrentLocal)
-      }
-    } else {
-      setstartDate(convertToUTC(_SearchDateRangeValue[0]))
-      setendDate(convertToUTC(_SearchDateRangeValue[1]))
-    }
-  }, [_selectedCollectionData])
-
-  useEffect(() => {
-    let correctStartSearchDate
-    let correctEndSearchDate
-
-    const StartDateAsDateObject =
-      startDate instanceof Date ? startDate : new Date(startDate)
-    const offsetInMilliseconds =
-      StartDateAsDateObject.getTimezoneOffset() * 60 * 1000
-
-    if (startDate instanceof Date) {
-      correctStartSearchDate = new Date(
-        startDate.getTime() - offsetInMilliseconds
-      ).toISOString()
-    } else {
-      correctStartSearchDate = new Date(startDate).toISOString()
+    if (!collectionStart) {
+      return
     }
 
-    if (endDate instanceof Date) {
-      correctEndSearchDate = new Date(
-        endDate.getTime() - offsetInMilliseconds
-      ).toISOString()
-    } else {
-      correctEndSearchDate = new Date(endDate).toISOString()
-    }
+    const collectionStartDate = dayjs(collectionStart)
+    const collectionEndDate = collectionEnd
+      ? dayjs(collectionEnd)
+      : dayjs() // Use current date if collection is ongoing
 
-    dispatch(
-      setSearchDateRangeValue([correctStartSearchDate, correctEndSearchDate])
-    )
-  }, [startDate, endDate])
+    const currentStartDate = dayjs(searchDateRangeValue[0])
+    const currentEndDate = dayjs(searchDateRangeValue[1])
 
-  useEffect(() => {
-    if (_hasLeftPanelTabChanged) {
-      setstartDate(convertToUTC(_SearchDateRangeValue[0]))
-      setendDate(convertToUTC(_SearchDateRangeValue[1]))
+    // Update date range if current range is completely outside collection extent
+    const isOutsideRange =
+      (currentStartDate.isBefore(collectionStartDate) &&
+        currentEndDate.isBefore(collectionStartDate)) ||
+      (currentStartDate.isAfter(collectionEndDate) &&
+        currentEndDate.isAfter(collectionEndDate))
+
+    if (isOutsideRange) {
+      dispatch(
+        setSearchDateRangeValue([
+          collectionStartDate.toISOString(),
+          collectionEndDate.toISOString()
+        ])
+      )
     }
-  }, [])
+  }, [selectedCollectionData, dispatch]) // Intentionally NOT including searchDateRangeValue to avoid loops
+
+  // Handle date changes - dispatch directly to Redux
+  const handleStartDateChange = (date) => {
+    if (date) {
+      // Create new Date at start of day in UTC
+      const utcDate = dayjs(date).startOf('day').toISOString()
+      dispatch(setSearchDateRangeValue([utcDate, searchDateRangeValue[1]]))
+    }
+  }
+
+  const handleEndDateChange = (date) => {
+    if (date) {
+      // Create new Date at end of day in UTC
+      const utcDate = dayjs(date).endOf('day').toISOString()
+      dispatch(setSearchDateRangeValue([searchDateRangeValue[0], utcDate]))
+    }
+  }
+
+  // Create display dates without mutation
+  const startDateForPicker = useMemo(() => {
+    const date = new Date(startDate)
+    date.setHours(0, 0, 0, 0)
+    return date
+  }, [startDate])
+
+  const endDateForPicker = useMemo(() => {
+    const date = new Date(endDate)
+    date.setHours(23, 59, 59, 999)
+    return date
+  }, [endDate])
 
   return (
-    <div className="datePicker">
-      <label>
-        Date Range <span className="datePickerLabelUTCText">(UTC)</span>{' '}
-        {!startDate || !endDate ? (
-          <span className="error-true">
-            <em>Required</em>
-          </span>
-        ) : null}
-      </label>
+    <Card height={116} label="Date Range (UTC)" className="datePicker">
+      {collectionExtentText && (
+        <div className="datePickerHelperText">{collectionExtentText}</div>
+      )}
       <div className="datePickerContainer">
-        <DatePicker
-          className="reactDatePicker"
-          selected={
-            startDate instanceof Date
-              ? startDate.setHours(0, 0, 0)
-              : new Date(startDate)
-          }
-          maxDate={endDate}
-          showPopperArrow={false}
-          todayButton="Today"
-          showIcon
-          icon={
-            <CalendarTodayIcon className="datePicker-icon"></CalendarTodayIcon>
-          }
-          toggleCalendarOnIconClick
-          closeOnScroll={true}
-          peekNextMonth
-          showMonthDropdown
-          showYearDropdown
-          dropdownMode="select"
-          dateFormat="yyyy-MM-dd"
-          popperPlacement="top-end"
-          onChange={(date) => setstartDate(date)}
-        ></DatePicker>
-        <span className="dateRangeSpanText">to</span>
-        <DatePicker
-          className="reactDatePicker"
-          selected={
-            endDate instanceof Date
-              ? endDate.setHours(23, 59, 59)
-              : new Date(endDate)
-          }
-          minDate={startDate}
-          maxDate={new Date()}
-          showPopperArrow={false}
-          todayButton="Today"
-          showIcon
-          icon={
-            <CalendarTodayIcon className="datePicker-icon"></CalendarTodayIcon>
-          }
-          toggleCalendarOnIconClick
-          closeOnScroll={true}
-          showMonthDropdown
-          showYearDropdown
-          dropdownMode="select"
-          dateFormat="yyyy-MM-dd"
-          popperPlacement="top-end"
-          onChange={(date) => setendDate(date)}
-        ></DatePicker>
+        <div className="datePickerInputWrapper">
+          <label htmlFor="startDatePicker" className="datePickerInputLabel">From</label>
+          <DatePicker
+            id="startDatePicker"
+            className="reactDatePicker"
+            selected={startDateForPicker}
+            maxDate={endDateForPicker}
+            showPopperArrow={false}
+            todayButton="Today"
+            showIcon
+            icon={
+              <CalendarTodayIcon className="datePicker-icon" />
+            }
+            toggleCalendarOnIconClick
+            closeOnScroll={true}
+            peekNextMonth
+            showMonthDropdown
+            showYearDropdown
+            dropdownMode="select"
+            dateFormat="yyyy-MM-dd"
+            popperPlacement="top-end"
+            onChange={handleStartDateChange}
+          />
+        </div>
+        <div className="datePickerInputWrapper">
+          <label htmlFor="endDatePicker" className="datePickerInputLabel">To</label>
+          <DatePicker
+            id="endDatePicker"
+            className="reactDatePicker"
+            selected={endDateForPicker}
+            minDate={startDateForPicker}
+            maxDate={new Date()}
+            showPopperArrow={false}
+            todayButton="Today"
+            showIcon
+            icon={
+              <CalendarTodayIcon className="datePicker-icon" />
+            }
+            toggleCalendarOnIconClick
+            closeOnScroll={true}
+            showMonthDropdown
+            showYearDropdown
+            dropdownMode="select"
+            dateFormat="yyyy-MM-dd"
+            popperPlacement="top-end"
+            onChange={handleEndDateChange}
+          />
+        </div>
       </div>
-    </div>
+    </Card>
   )
 }
 
