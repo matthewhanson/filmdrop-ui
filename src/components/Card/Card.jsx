8fc0b799ea64f33734608b6065f31c92a657f014
pjhartzell <preston.hartzell@gmail.com>
2026-01-27 09:44:33 -0600

    Card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Card.css'
+
+const Card = ({ height = 'auto', label, children, className = '' }) => {
+  const style = height !== 'auto' ? { height: `${height}px` } : {}
+
+  return (
+    <div className={`Card ${className}`} style={style}>
+      {label && <label className="Card__label">{label}</label>}
+      <div className="Card__content">{children}</div>
+    </div>
+  )
+}
+
+Card.propTypes = {
+  height: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.oneOf(['auto'])
+  ]),
+  label: PropTypes.string,
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string
+}
+
+export default Card
